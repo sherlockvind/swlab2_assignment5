816a9da436de7dcd65cbfa5daefbe8ecf6ed496d
argongs <gauravisvirtual@yahoo.com>
2021-04-01 20:04:48 +0530

    SensorData/main/utils.py
    added summarise function to combine results of other functions

--- SensorData/main/utils.py
+++ SensorData/main/utils.py
@@ -2,49 +2,65 @@ import numpy as np
 import pandas as pd
 from scipy.signal import argrelextrema
 
+def maximum(df, field):
+    # Calculate maximum of input data frame 'df' for the given 'field'
+    return df[field].max()
 
-def maximum(df):
-    return df['quantity'].max()
+def minimum(df, field):
+    # Calculate minimum of input data frame 'df' for the given 'field'
+    return df[field].min()
 
-def minimum(df):
-    return df['quantity'].min()
+def count_relative_minima(df, options):
+    field, n = options
+    # n : number of points to be checked before and after
+    # field : field of the data on which this method has to be applied upon
+    return argrelextrema(df[field].to_numpy(), np.less_equal, order=n)[0].shape[0]
 
-def count_relative_minima(df, n=5):
-    # n : number of points to be checked before and after  
-    return argrelextrema(df.quantity.values, np.less_equal, order=n)[0].shape[0]
-
-def count_relative_maxima(df, n=5):
-    # n : number of points to be checked before and after    
-    return argrelextrema(df.quantity.values, np.greater_equal, order=n)[0].shape[0]
-    
+def count_relative_maxima(df, options):
+    field, n = options
+    # n : number of points to be checked before and after
+    # field : field of the data on which this method has to be applied upon
+    return argrelextrema(df[field].to_numpy(), np.greater_equal, order=n)[0].shape[0]
 
 def calc_slopes(data_frame, column_names):
-        time_field, reading_field = column_names
-        # Convert required column data to numpy array before slope calculation
-        t1 = data_frame[time_field].to_numpy()[0:-1]
-        t2 = data_frame[time_field].to_numpy()[1:]
-        avg_time_period = (t2-t1).mean()
-        #print(t1, t2)
-        #print(avg_time_period)
+    time_field, reading_field = column_names
+    t1 = data_frame[time_field].to_numpy()[0:-1]
+    t2 = data_frame[time_field].to_numpy()[1:]
+    avg_time_period = (t2-t1).mean()
+    #print(t1, t2)
+    #print(avg_time_period)
 
-        r1 = data_frame[reading_field].to_numpy()[0:-1]
-        r2 = data_frame[reading_field].to_numpy()[1:]
-        reading_difference = r2-r1
-        #print(reading_difference)
+    r1 = data_frame[reading_field].to_numpy()[0:-1]
+    r2 = data_frame[reading_field].to_numpy()[1:]
+    reading_difference = r2-r1
+    #print(reading_difference)
 
-        negative_difference = reading_difference[reading_difference < 0]
-        positive_difference = reading_difference[reading_difference > 0]
+    negative_difference = reading_difference[reading_difference < 0]
+    positive_difference = reading_difference[reading_difference > 0]
 
-        avg_negative_diff = 0
-        avg_positive_diff = 0
-        
-        if negative_difference.size > 0:
-            avg_negative_diff = negative_difference.mean()
+    avg_negative_diff = 0
+    avg_positive_diff = 0
 
-        if positive_difference.size > 0:
-            avg_positive_diff = positive_difference.mean() 
-        
-        avg_downward_slope = avg_negative_diff / avg_time_period
-        avg_upward_slope = avg_positive_diff / avg_time_period
+    if negative_difference.size > 0:
+        avg_negative_diff = negative_difference.mean()
 
-        return (avg_downward_slope, avg_upward_slope)
+    if positive_difference.size > 0:
+        avg_positive_diff = positive_difference.mean()
+
+    avg_downward_slope = avg_negative_diff / avg_time_period
+    avg_upward_slope = avg_positive_diff / avg_time_period
+
+    return (avg_downward_slope, avg_upward_slope)
+
+def summarise(df, options):
+    time_field, reading_field, sensor_field, neighbourhood_size = options
+
+    max_val = maximum(df, reading_field)
+    min_val = minimum(df, reading_field)
+    rel_max_count = count_relative_maxima(df, (reading_field, neighbourhood_size))
+    rel_min_count = count_relative_minima(df, (reading_field, neighbourhood_size))
+    (avg_dec_slope, avg_inc_slope) = calc_slopes(df, (time_field, reading_field))
+
+    summarised_dict = {"maximum": max_val, "minimum": min_val, "maxima_count": rel_max_count, "minima_count": rel_min_count, "avg_inc_slope": avg_inc_slope, "avg_dec_slope": avg_dec_slope, "sensor": df[sensor_field].unique()[0]}
+
+    return summarised_dict
